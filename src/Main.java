d262cb031efc7b127f60a3ed49f53efd6ddedd9c
Nuttapong Klaokliang <nuttapong95.klng@gmail.com>
2025-07-24 19:31:39 +0700

    Add input validation with while loop

--- src/Main.java
+++ src/Main.java
@@ -8,14 +8,32 @@ public class Main {
 
         Scanner scanner = new Scanner(System.in);
 
-        System.out.print("Principal: ");
-        int principal = scanner.nextInt();
+        int principal;
+        while (true) {
+            System.out.print("Principal (1K - 1M): ");
+            principal = scanner.nextInt();
+            if (principal >= 1000 && principal <= 1_000_000)
+                break;
+            System.out.println("Enter a number between 1,000 and 1,000,000.");
+        }
 
-        System.out.print("Annual Interest Rate: ");
-        float annualInterest = scanner.nextFloat();
+        float annualInterest;
+        while (true) {
+            System.out.print("Annual Interest Rate: ");
+            annualInterest = scanner.nextFloat();
+            if (annualInterest > 0 && annualInterest <= 30)
+                break;
+            System.out.println("Enter a value greater than 0 and less than or equal to 30.");
+        }
 
-        System.out.print("Period (Years): ");
-        int period = scanner.nextInt();
+        int period;
+        while (true) {
+            System.out.print("Period (Years): ");
+            period = scanner.nextInt();
+            if (period >= 1 && period <= 30)
+                break;
+            System.out.println("Enter a value between 1 and 30.");
+        }
 
         float monthlyInterest = annualInterest / PERCENT / MONTH_IN_YEAR;
         int PayingMonths = period * MONTH_IN_YEAR;
